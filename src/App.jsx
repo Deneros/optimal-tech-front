2207ed2cba473481f9c557e073822aa999dd1f61
Nicolas Velez <nicolas.velez99@hotmail.com>
2024-10-22 15:05:37 -0500

    [feat]: Centralizacion de aplicacion con enrutamiento

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import Login from './pages/Auth/Login';
+import Register from './pages/Auth/Register';
+import Home from './pages/Home/Home';
+
+function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      setIsAuthenticated(true);
+    }
+  }, []);
+
+  const handleLogin = (token) => {
+    localStorage.setItem('token', token);
+    setIsAuthenticated(true);
+  };
+
+  const handleLogout = () => {
+    localStorage.removeItem('token');
+    setIsAuthenticated(false);
+  };
+
+  return (
+    <Router>
+      <div>
+      <nav>
+        {!isAuthenticated ? (
+          <>
+            <a href="/login">Login</a> | <a href="/register">Registro</a>
+          </>
+        ) : (
+          <button onClick={handleLogout}>Cerrar sesión</button>
+        )}
+      </nav>
+
+        <Routes>
+          <Route path="/login" element={<Login onLogin={handleLogin} />} />
+          <Route path="/register" element={<Register />} />
+          <Route
+            path="/home"
+            element={isAuthenticated ? <Home /> : <Navigate to="/login" />}
+          />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
